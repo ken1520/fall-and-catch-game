88806aabe98e5c29fb3304f1b3a4873911e48665
Ken LEE <ken.lee@shopline.com>
2024-08-23 11:45:41 +0800

    feat: add home page and leaderboard page

--- index.js
+++ index.js
@@ -5,8 +5,19 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 const port = 3000;
 
+// Route to serve the home page
 app.get('/', (req, res) => {
+  res.render('index', req.query);
+});
+
+// Route to serve the game
+app.get('/game', (req, res) => {
   res.render('game', req.query);
+});
+
+// Route to serve the leaderboard
+app.get('/leaderboard', (req, res) => {
+  res.render('leaderboard', req.query);
 });
 
 // Route to serve images
